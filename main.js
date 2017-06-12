3e0b4d841a05d22146a7fcc09e6653c4011a1fe3
xu.miche@gmail.com <xu.miche@gmail.com>
2017-06-12 10:14:58 -0700

    Created span tags for each character #2

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
+var $phrase = document.querySelector('#exercise1')
+
 var phrase = '"Life is short. Smile while you still have teeth."'
 var chars = phrase.split('')
+
+for (var i = 0; i < chars.length; i++) {
+  var $char = document.createElement('span')
+  $char.textContent = chars[i]
+  $phrase.appendChild($char)
+}
+
+
+
+
+// Create span tags in DOM
+// Put each character in individual span tags
+// append each of those span tags to desired element
