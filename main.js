d1d28b3a8ac6047b64f7aa1801113277c23c4b05
xu.miche@gmail.com <xu.miche@gmail.com>
2017-06-13 12:24:17 -0700

    Created div tag and positioned it.

--- main.js
+++ main.js
@@ -12,13 +12,31 @@ for (var i = 0; i < chars.length; i++) {
 $phrase.querySelector('span').classList.add('current')
 var $currentChar = document.querySelector('.current')
 
+var $score = document.createElement('div')
+$phrase.appendChild($score)
+$phrase.querySelector('div').classList.add('score')
+var $showScore = document.querySelector('.score')
+
 document.addEventListener('keypress', function(event) {
     if ($currentChar === null) {
-      alert('Good job!')
+    //var $correct = document.querySelectorAll('span.correct').length
+      var $wrong = document.querySelectorAll('span.wrong').length
+    //$correct = document.querySelectorAll('span').length - $wrong
+      console.log($wrong)
+    //console.log($correct)
     } else if (event.key === $currentChar.textContent) {
       $currentChar.classList.add('correct')
       $currentChar = $currentChar.nextSibling
+      console.log('correct')
     } else {
       $currentChar.classList.add('wrong')
+      console.log('wrong')
     }
 })
+
+
+
+// Step 1 Count the number or errors and correct inputs
+// Step 2 The count needs to be displayed for the user
+
+// total characters - # wrong = # correct
