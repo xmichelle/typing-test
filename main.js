fc4309c14249e9f19595c2d68fb126da23512c8c
xu.miche@gmail.com <xu.miche@gmail.com>
2017-06-13 12:45:34 -0700

    Updated div element and added statement that indicates typing error at the end of the test. #4

--- main.js
+++ main.js
@@ -12,19 +12,13 @@ for (var i = 0; i < chars.length; i++) {
 $phrase.querySelector('span').classList.add('current')
 var $currentChar = document.querySelector('.current')
 
-var $score = document.createElement('div')
-$phrase.appendChild($score)
-$phrase.querySelector('div').classList.add('score')
 var $showScore = document.querySelector('.score')
 
 document.addEventListener('keypress', function(event) {
     if ($currentChar === null) {
-    //var $correct = document.querySelectorAll('span.correct').length
       var $wrong = document.querySelectorAll('span.wrong').length
-    //$correct = document.querySelectorAll('span').length - $wrong
       console.log($wrong)
-    //console.log($correct)
-    //
+      $showScore.textContent = '# of incorrect characters: ' + $wrong
     } else if (event.key === $currentChar.textContent) {
       $currentChar.classList.add('correct')
       $currentChar = $currentChar.nextSibling
@@ -34,10 +28,3 @@ document.addEventListener('keypress', function(event) {
       console.log('wrong')
     }
 })
-
-
-
-// Step 1 Count the number or errors and correct inputs
-// Step 2 The count needs to be displayed for the user
-
-// total characters - # wrong = # correct
